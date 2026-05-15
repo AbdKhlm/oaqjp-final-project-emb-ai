05bcea7e0bf857744e4fdc61b8d2ff7598d3fbf7
AbdKhlm <abdulkhalim0805@gmail.com>
2026-05-16 02:22:53 +0700

    Updated mywebscript.js

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -4,7 +4,7 @@ let RunSentimentAnalysis = ()=>{
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
+            document.getElementById("system_response").innerHTML = "<p>" + xhttp.responseText + "</p>";
         }
     };
     xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
